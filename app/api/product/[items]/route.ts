3aaf7b2894fff5db0bb7fc7dafee95f596cde9be
isharaLiyanage <ishandananjaya1111@gmail.com>
2024-08-31 19:08:48 +0530

    change

--- app/api/product/[items]/route.ts
+++ app/api/product/[items]/route.ts
@@ -1,9 +1,9 @@
 import prisma from "@/utils/connect";
-import { NextApiRequest, NextApiResponse } from "next";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
-  req: NextApiRequest,
+  req: NextRequest,
   { params }: { params: { items: string } }
 ) => {
   const items = params.items;
