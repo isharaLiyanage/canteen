136d92333a55df60ca0492a0d80c992c37718696
isharaLiyanage <ishandananjaya1111@gmail.com>
2024-08-31 09:30:27 +0530

    v

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    customKey: "my-value",
-    PAYPAL_ID:
-      "Aa27r4laM_ZpsZ2VRxsHX2Si2_SBew2l1KGMfSjs_WQQxzAGMsD_fuwtG9FrO3xYHYpq7fKYBI_73uLh",
-    PAYPAL_KEY:
-      "EA3hlryDoo3avZoeTzzmoRm3XfBzfpoYBRTmvCgBS8VUP2VZ0jqLxRnhx8-7wRzPdNRwhHZQkhSd9xR5",
-  },
+
 };
 
 export default nextConfig;
